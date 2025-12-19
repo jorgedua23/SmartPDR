734dede8260e4e621327f944af67f1bbe242eabc
jorgedua23 <jorgedua@gmail.com>
2025-12-19 12:28:11 -0500

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -3,12 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+const init = () => {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+};
+
+// Pequeña espera para asegurar que el DOM y el shim estén sincronizados
+if (document.readyState === 'complete') {
+  init();
+} else {
+  window.addEventListener('load', init);
 }
